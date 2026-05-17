d14e4bc0a9556b16599e531dad5feffc4c5b0124
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-05-17 19:38:56 -0300

    Adicionando a pasta de pages e login

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import {Text, View} from "react-native";
+import { style } from "./styles";
+
+
+export default function Login() {
+    return (
+        <View style={style.container}>
+            <View style={style.boxTop}></View>
+            <View style={style.boxMid}></View>
+            <View style={style.boxBottom}></View>
+                
+        </View>
+       
+    )
+}
---
+++ src/pages/login/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+
+    container:{
+
+    },
+
+    boxTop:{
+
+    },
+
+    boxMid:{
+
+    },
+
+    boxBottom:{
+        
+    }
+
+});
+
