cdad1d3e8bfc96d86a43a2a07913b9837b826d0d
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-05-18 19:41:51 -0300

    Adicionando o Icone da tela de login e as cores do projeto

---
+++ src/global/themes.tsx
@@ -0,0 +1,10 @@
+export const themes = {
+    colors:{
+        primary:'#878AF6',
+        secondary:'#FFFFFF',
+        lightGray:'#D7D8D7',
+        gray:'gray',
+        bgScreen:'#F1F7FA',
+        white:'#FFFFFF',
+    }
+}
