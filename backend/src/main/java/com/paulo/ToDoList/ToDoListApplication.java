c3c316d788e846fa423f789850804100b9152df6
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-05-19 22:06:00 -0300

    Adicioando a estrutura do projeto no gitHub

---
+++ backend/src/main/java/com/paulo/ToDoList/ToDoListApplication.java
@@ -0,0 +1,28 @@
+package com.paulo.ToDoList;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApplication {
+
+	public static void main(String[] args) {
+
+		Dotenv dotenv  = Dotenv.configure()
+						.filename(".env")
+						.load();
+
+
+		//Define variáveis de ambiente no sistema
+		System.setProperty("SPRING_PROFILE", dotenv.get("SPRING_PROFILE"));
+		System.setProperty("SERVER_PORT",dotenv.get("SERVER_PORT"));
+		System.setProperty("DB_URL",dotenv.get("DB_URL"));
+		System.setProperty("DB_USER",dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD",dotenv.get("DB_PASSWORD"));
+
+
+		SpringApplication.run(ToDoListApplication.class, args);
+	}
+
+}
