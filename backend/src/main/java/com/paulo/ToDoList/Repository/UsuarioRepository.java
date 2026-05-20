1c417f7df1c089c58f4d08a40a7f84c998ba42e0
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-05-19 22:02:51 -0300

    Modificado a classe de Repository do usuario

--- backend/src/main/java/com/paulo/ToDoList/Repository/UsuarioRepository.java
+++ backend/src/main/java/com/paulo/ToDoList/Repository/UsuarioRepository.java
@@ -1,4 +1,15 @@
 package com.paulo.ToDoList.Repository;
 
-public interface UsuarioRepository {
+import com.paulo.ToDoList.Entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+
+
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
