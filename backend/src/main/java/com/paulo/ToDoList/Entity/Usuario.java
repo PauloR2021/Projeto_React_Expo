6c2f8d39fdbc7929ecddbdffcec5a95654eb87dd
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-05-19 21:59:26 -0300

    Modificando o entity do usuario

--- backend/src/main/java/com/paulo/ToDoList/Entity/Usuario.java
+++ backend/src/main/java/com/paulo/ToDoList/Entity/Usuario.java
@@ -1,4 +1,29 @@
 package com.paulo.ToDoList.Entity;
 
-public class Usuario {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "usuarios")
+@Getter @Setter
+public class Usuario{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private Boolean ativo = true;
+
 }
