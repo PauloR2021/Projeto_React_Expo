10ee3ec86dda077815712a9bd6f0af3bade86f93
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-05-19 22:00:52 -0300

    Modificando as classes de exeception

--- backend/src/main/java/com/paulo/ToDoList/Exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/paulo/ToDoList/Exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,44 @@
 package com.paulo.ToDoList.Exceptions;
 
+
+import com.paulo.ToDoList.Api.RetornoApi;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.List;
+
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<RetornoApi<Void>> resourceNotFoundException(ResourceNotFoundException ex){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new RetornoApi<>(false,ex.getMessage(),null));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RetornoApi<Void>> methodArgumentNotValidException(MethodArgumentNotValidException ex){
+        List<String> fieldErrors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage).toList();
+
+        String mensagem = String.join(",", fieldErrors);
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(new RetornoApi<>(false, mensagem,null));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<RetornoApi<Void>> runtimeException(RuntimeException ex){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new RetornoApi<>(false,ex.getMessage(),null));
+    }
 }
--- backend/src/main/java/com/paulo/ToDoList/Exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/paulo/ToDoList/Exceptions/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.paulo.ToDoList.Exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-  public ResourceNotFoundException(String message) {
-    super(message);
-  }
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
