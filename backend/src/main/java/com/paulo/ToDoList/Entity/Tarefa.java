9204fcdb8bd6a52af98651aad4b7581ee4c8655a
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-05-21 07:22:26 -0300

    Criando a Entity da tarefa

---
+++ backend/src/main/java/com/paulo/ToDoList/Entity/Tarefa.java
@@ -0,0 +1,24 @@
+package com.paulo.ToDoList.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+
+@Entity
+@Table(name = "tarefas")
+@Getter @Setter
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+    private String status;
+    private Timestamp dataCriacao;
+
+}
