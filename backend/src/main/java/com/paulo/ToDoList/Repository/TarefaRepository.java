e29f49f674af961c782a37d8589e1174bbd7f5d7
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-05-29 07:33:07 -0300

    Ajustando o repository das tarefas

--- backend/src/main/java/com/paulo/ToDoList/Repository/TarefaRepository.java
+++ backend/src/main/java/com/paulo/ToDoList/Repository/TarefaRepository.java
@@ -3,8 +3,10 @@ package com.paulo.ToDoList.Repository;
 import com.paulo.ToDoList.Entity.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 
-    boolean existsById(String titulo);
+    List<Tarefa> findByIdUsuario(Long idUsuario);
 
 }
