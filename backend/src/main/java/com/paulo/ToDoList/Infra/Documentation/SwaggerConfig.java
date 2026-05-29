971acf9ca2386c3f99de334c9fc90b87a5e51bd3
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-05-29 07:32:15 -0300

    Ajustando a documentacao da API

--- backend/src/main/java/com/paulo/ToDoList/Infra/Documentation/SwaggerConfig.java
+++ backend/src/main/java/com/paulo/ToDoList/Infra/Documentation/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.paulo.ToDoList.Infra.Documentation;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,12 +15,18 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                .name("Bearer Authentication")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")        // ✅ lowercase
+                                .bearerFormat("JWT")
+                        )
+                )
                 .info(new Info()
-                        .title("API React - App ToDoList")
-                        .description("API para App ToDoList")
+                        .title("ToDoList API")
                         .version("1.0")
                 );
-
-
     }
 }
