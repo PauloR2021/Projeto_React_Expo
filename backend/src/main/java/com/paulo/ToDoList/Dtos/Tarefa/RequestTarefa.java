2de07da297cb38f846e3eda886d239f29b2c832d
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-05-29 07:31:47 -0300

    Adicionando o request das tarefas

--- backend/src/main/java/com/paulo/ToDoList/Dtos/Tarefa/RequestTarefa.java
+++ backend/src/main/java/com/paulo/ToDoList/Dtos/Tarefa/RequestTarefa.java
@@ -3,7 +3,7 @@ package com.paulo.ToDoList.Dtos.Tarefa;
 import jakarta.validation.constraints.NotBlank;
 
 public record RequestTarefa(
-        @NotBlank(message = "Necessita de uma Id para Usuário")
+
         Long idUsuario,
         String nome,
         String titulo,
